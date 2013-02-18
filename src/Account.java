c4ad85c3b0fd4d892fa9094a01fca432728bc5c1
fbrossler <info@cross-stream.de>
2013-02-18 16:27:25 +0100

    implemented Account Class with the basic funktion deposit and withdraw

--- src/Account.java
+++ src/Account.java
@@ -4,8 +4,39 @@
 
 /**
  * @author Felix Brossler
- *
+ * 
  */
 public class Account {
+	private static final String NO_OWNER = "No Owner";
+	private static final double DEFAULT_BALACE = 0.0;
+	private double balance;
+	private final String owner;
+
+	public Account() {
+		this.owner = NO_OWNER;
+		this.balance = DEFAULT_BALACE;
+	}
+
+	public double getBalance() {
+		return balance;
+	}
+
+	public String getOwner() {
+		return owner;
+	}
+
+	public void deposit(double money) {
+		if (money >= 0.0) {
+			this.balance += money;
+		} else {
+
+		}
+	}
+
+	public void withdraw(double money) {
+		if (money > 0.0) {
+			this.balance -= money;
+		}
+	}
 
 }
