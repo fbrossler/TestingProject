7e91726fadc1ee580110cb3d843f28b95e676daa
fbrossler <info@cross-stream.de>
2013-03-03 17:31:22 +0100

    foo

--- src/bank/AccountTest.java
+++ src/bank/AccountTest.java
@@ -7,12 +7,9 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
 
-
 import org.junit.Before;
 import org.junit.Ignore;
 import org.junit.Test;
-
-import bank.Account;
 
 /**
  * @author Felix Brossler
